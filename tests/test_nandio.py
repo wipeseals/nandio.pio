421b1c1c03991d69579d3334c260b3aedf9a8eb7
wipeseals <wipeseals@gmail.com>
2025-05-25 00:22:56 +0900

    test: assert_csメソッドの追加とテストケースの追加

--- tests/test_nandio.py
+++ tests/test_nandio.py
@@ -7,6 +7,7 @@ from src.nandio import PIN_DIR_WRITE, CmdBuilder, NandCommandId, PioCmdId, Util
 class TestCmdBuilder:
     def test_init_pin(self):
         payload: List[int] = CmdBuilder.init_pin()
+
         # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
         # arg_0 = { arg0[31:0] }
         assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
@@ -14,6 +15,17 @@ class TestCmdBuilder:
             0x00000000 | Util.bitmerge_cs(0x00, None)
         )  # CS0/1ともに非選択
 
+    @pytest.mark.parametrize(
+        "cs",
+        [0, 1, None],
+    )
+    def test_assert_cs(self, cs: int | None):
+        payload: List[int] = CmdBuilder.assert_cs(cs)
+
+        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
+        assert payload[0x1] == (0x00000000 | Util.bitmerge_cs(0x00, cs))
+
+    
     @pytest.mark.parametrize(
         "cs",
         [0, 1],
