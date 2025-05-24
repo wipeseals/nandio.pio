3a3f436ff0a7f961c44d8509ea5cf052c4f2011c
wipeseals <wipeseals@gmail.com>
2025-05-25 00:21:42 +0900

    refactor: TestCmdBuilderクラスの整理とテストの改善

--- tests/test_nandio.py
+++ tests/test_nandio.py
@@ -1,25 +1,27 @@
+from typing import List
 import pytest
 import itertools
 from src.nandio import PIN_DIR_WRITE, CmdBuilder, NandCommandId, PioCmdId, Util
 
 
-def test_create_reset_payload():
-    cs = 0
-    tx_fifo = list(
-        itertools.chain.from_iterable(
-            [
-                CmdBuilder.init_pin(),
-                CmdBuilder.assert_cs(select_cs=cs),
-                CmdBuilder.cmd_latch(
-                    cmd=NandCommandId.Reset,
-                    select_cs=cs,
-                ),
-                CmdBuilder.deassert_cs(select_cs=cs),
-            ]
-        )
-    )
+class TestCmdBuilder:
+    def test_init_pin(self):
+        payload: List[int] = CmdBuilder.init_pin()
+        # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
+        # arg_0 = { arg0[31:0] }
+        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
+        assert payload[0x1] == (
+            0x00000000 | Util.bitmerge_cs(0x00, None)
+        )  # CS0/1ともに非選択
 
-    # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
-    assert tx_fifo[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
-    # arg_0 = { arg0[31:0] }
-    assert tx_fifo[0x1] == (0x00000000 | Util.bitmerge_cs(0x00, None))
+    @pytest.mark.parametrize(
+        "cs",
+        [0, 1],
+    )
+    def test_create_reset_payload(self, cs: int):
+        payload: List[int] = CmdBuilder.seq_reset(cs)
+
+        # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
+        # arg_0 = { arg0[31:0] }
+        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
+        assert payload[0x1] == (0x00000000 | Util.bitmerge_cs(0x00, None))
