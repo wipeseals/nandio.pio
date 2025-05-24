3c92bc51ab9e2e8e1beba5b01a2ff7108ad8bd6c
wipeseals <wipeseals@gmail.com>
2025-05-25 00:28:49 +0900

    test: cmd0 の定義を共通化

--- tests/test_nandio.py
+++ tests/test_nandio.py
@@ -5,15 +5,24 @@ from src.nandio import PIN_DIR_WRITE, CmdBuilder, NandCommandId, PioCmdId, Util
 
 
 class TestCmdBuilder:
+    @staticmethod
+    def cmd0(
+        cmd: PioCmdId,
+        dir: int,
+        count: int,
+    ) -> int:
+        """
+        Encode the first command word for Test.
+        `cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }`
+        """
+        return (cmd << 28) | ((count - 1) << 16) | dir
+
     def test_init_pin(self):
         payload: List[int] = CmdBuilder.init_pin()
 
-        # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
         # arg_0 = { arg0[31:0] }
-        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
-        assert payload[0x1] == (
-            0x00000000 | Util.bitmerge_cs(0x00, None)
-        )  # CS0/1ともに非選択
+        assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
+        assert payload[0x1] == Util.bitmerge_cs(0x00, None)
 
     @pytest.mark.parametrize(
         "cs",
@@ -22,18 +31,11 @@ class TestCmdBuilder:
     def test_assert_cs(self, cs: int | None):
         payload: List[int] = CmdBuilder.assert_cs(cs)
 
-        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
-        assert payload[0x1] == (0x00000000 | Util.bitmerge_cs(0x00, cs))
+        assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
+        assert payload[0x1] == Util.bitmerge_cs(0x00, cs)
 
-    
-    @pytest.mark.parametrize(
-        "cs",
-        [0, 1],
-    )
-    def test_create_reset_payload(self, cs: int):
-        payload: List[int] = CmdBuilder.seq_reset(cs)
+    def test_deassert_cs(self):
+        payload: List[int] = CmdBuilder.deassert_cs()
 
-        # cmd_0 = { cmd_id[3:0], transfer_count[11:0], pindirs[15:0] }
-        # arg_0 = { arg0[31:0] }
-        assert payload[0x0] == (PioCmdId.Bitbang << 28) | (0x0000 << 16) | PIN_DIR_WRITE
-        assert payload[0x1] == (0x00000000 | Util.bitmerge_cs(0x00, None))
+        assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
+        assert payload[0x1] == Util.bitmerge_cs(0x00, None)
