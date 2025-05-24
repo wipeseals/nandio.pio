cf510878915a6dcb7dbaec1d10dfa2bd34c39526
wipeseals <wipeseals@gmail.com>
2025-05-24 21:38:39 +0900

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+    description = "A flake for a Python and MicroPython development environment";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+     {
+        devShell.x86_64-linux = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+                python312
+                python312.pkgs.uv
+                python312.pkgs.pytest   
+                micropython             
+            ];
+        };
+    };
+}
