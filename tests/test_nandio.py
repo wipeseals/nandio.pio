1fff4ea2651f50a790f4844862fd1a1192a08a75
wipeseals <wipeseals@gmail.com>
2025-05-25 21:54:09 +0900

    test: add test_addr_latch

--- tests/test_nandio.py
+++ tests/test_nandio.py
@@ -1,10 +1,9 @@
-from typing import List
 import pytest
-import itertools
-from src.nandio import PIN_DIR_WRITE, CmdBuilder, NandCommandId, PioCmdId, Util
+from typing import List
+from src.nandio import PIN_DIR_WRITE, PioCmdBuilder, NandCommandId, PioCmdId, Util
 
 
-class TestCmdBuilder:
+class TestPioCmdBuilder:
     @staticmethod
     def cmd0(
         cmd: PioCmdId,
@@ -18,27 +17,26 @@ class TestCmdBuilder:
         return (cmd << 28) | ((count - 1) << 16) | dir
 
     def test_init_pin(self):
-        payload: List[int] = CmdBuilder.init_pin()
+        payload: List[int] = PioCmdBuilder.init_pin()
 
-        # arg_0 = { arg0[31:0] }
         assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
-        assert payload[0x1] == Util.bitmerge_cs(0x00, None)
+        assert payload[0x1] == Util.bitor_cs(0x00, None)
 
     @pytest.mark.parametrize(
         "cs",
         [0, 1, None],
     )
     def test_assert_cs(self, cs: int | None):
-        payload: List[int] = CmdBuilder.assert_cs(cs)
+        payload: List[int] = PioCmdBuilder.assert_cs(cs)
 
         assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
-        assert payload[0x1] == Util.bitmerge_cs(0x00, cs)
+        assert payload[0x1] == Util.bitor_cs(0x00, cs)
 
     def test_deassert_cs(self):
-        payload: List[int] = CmdBuilder.deassert_cs()
+        payload: List[int] = PioCmdBuilder.deassert_cs()
 
         assert payload[0x0] == self.cmd0(PioCmdId.Bitbang, PIN_DIR_WRITE, 1)
-        assert payload[0x1] == Util.bitmerge_cs(0x00, None)
+        assert payload[0x1] == Util.bitor_cs(0x00, None)
 
     @pytest.mark.parametrize(
         "cs",
@@ -49,7 +47,23 @@ class TestCmdBuilder:
         [NandCommandId.Reset, NandCommandId.ReadId],
     )
     def test_cmd_latch(self, cs: int | None, cmd: int):
-        payload: List[int] = CmdBuilder.cmd_latch(cmd, cs)
+        payload: List[int] = PioCmdBuilder.cmd_latch(cmd, cs)
 
         assert payload[0x0] == self.cmd0(PioCmdId.CmdLatch, PIN_DIR_WRITE, 1)
-        assert payload[0x1] == Util.bitmerge_cs(cmd, cs)
+        assert payload[0x1] == Util.bitor_cs(cmd, cs)
+
+    @pytest.mark.parametrize(
+        "cs",
+        [0, 1],
+    )
+    @pytest.mark.parametrize(
+        "addrs",
+        [[0xAA, 0x99, 0x55, 0x66], [0x11, 0x22]],
+    )
+    def test_addr_latch(self, cs: int, addrs: List[int]):
+        payload: List[int] = PioCmdBuilder.addr_latch(addrs, cs)
+
+        assert payload[0x0] == self.cmd0(PioCmdId.AddrLatch, PIN_DIR_WRITE, len(addrs))
+        assert payload[0x1] == 0x00  # don't care
+        for i, addr in enumerate(addrs):
+            assert payload[i + 2] == Util.bitor_cs(addr, cs)
