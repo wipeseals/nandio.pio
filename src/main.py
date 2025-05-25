2ddd612f13b7c33fc3d7dee480be3907be071efb
wipeseals <wipeseals@gmail.com>
2025-05-26 00:48:56 +0900

    feat: implement sim_all command for PIO simulation with scenarios

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+from dataclasses import dataclass
+from typing import List
 import rich_click as click
 from pathlib import Path
 import logging
 from rich.logging import RichHandler
+from src.nandio_pio import PioCmdBuilder
+from rich.progress import Progress
+
+from src.simulator import Result, Simulator
 
 
 @click.group()
@@ -25,10 +31,11 @@ def cli(
 
 
 @cli.command()
-@click.argument(
-    "pio_path",
+@click.option(
+    "--pio_path",
     required=True,
     type=click.Path(exists=True, path_type=Path),
+    default="nandio.pio",
 )
 @click.option(
     "--output_path",
@@ -36,12 +43,47 @@ def cli(
     type=click.Path(writable=True, path_type=Path),
     default="output",
 )
-def sim(
+@click.option(
+    "--cs",
+    required=True,
+    type=int,
+    default=0,
+)
+def sim_all(
     pio_path: Path,
     output_path: Path,
+    cs: int,
 ):
-    """Run the PIO simulation."""
-    logging.debug(f"{pio_path=}, {output_path=}")
+    """Simulate all PioCmdBuilder sequences."""
+
+    @dataclass
+    class SimScenario:
+        name: str
+        payload: list[int]
+        test_cycles: int = 100
+
+    program_str = Path(pio_path).read_text(encoding="utf-8")
+    scenarios: List[SimScenario] = [
+        SimScenario("reset", PioCmdBuilder.seq_reset(cs=cs), test_cycles=40),
+        SimScenario("read_id", PioCmdBuilder.seq_read_id(cs=cs), test_cycles=100),
+    ]
+
+    with Progress() as progress:
+        task = progress.add_task("Simulating all scenarios...", total=len(scenarios))
+        for scenario in scenarios:
+            ret: Result = Simulator.execute(
+                program_str=program_str,
+                test_cycles=scenario.test_cycles,
+                tx_fifo_entries=scenario.payload,
+            )
+            output_dir = output_path / scenario.name
+            output_dir.mkdir(parents=True, exist_ok=True)
+            ret.save(output_dir)
+            click.secho(f" ... '{scenario.name}' {scenario.test_cycles}cyc")
+            progress.advance(task)
+    click.secho(
+        f"All simulations completed successfully. output saved to {output_path.absolute()}"
+    )
 
 
 if __name__ == "__main__":
