2342ad474db7adc2a4ead79671ec06191fa5881e
wipeseals <wipeseals@gmail.com>
2025-06-05 22:57:57 +0900

    feat: impl read_page

--- main.py
+++ main.py
@@ -332,15 +332,55 @@ class PioNandCommander:
         rx_dma0.close()
         return rx_data
 
-    def read_page(
+    async def read_page(
         self,
-        chip_index: int,
+        cs: int,
         block: int,
         page: int,
         col: int = 0,
         num_bytes: int = NandConfig.PAGE_ALL_BYTES,
     ) -> bytearray | None:
-        pass
+        sm0 = self._setup_pio0_nandio()
+        sm0.active(1)
+
+        # TX Payload
+        tx_payload = array.array("I")
+        PioCmdBuilder.seq_read(
+            tx_payload,
+            cs=cs,
+            column_addr=col,
+            page_addr=page,
+            block_addr=block,
+            data_count=num_bytes,
+        )
+        tx_dma0 = self._setup_tx_dma(
+            dreq=Dreq.PIO0_SM0_TX_DREQ, sm=sm0, tx_payload=tx_payload
+        )
+        tx_dma0.active(1)
+
+        # RX Data
+        rx_data = bytearray(num_bytes)
+        rx_dma0 = self._setup_rx_dma(
+            dreq=Dreq.PIO0_SM0_RX_DREQ, sm=sm0, rx_data=rx_data, num_bytes=num_bytes
+        )
+        rx_dma0.active(1)
+
+        # wait for finished
+        start_ms = utime.ticks_ms()
+        while rx_dma0.active():
+            await uasyncio.sleep_ms(1)
+            elapsed_ms = utime.ticks_diff(utime.ticks_ms(), start_ms)
+            if elapsed_ms > self._timeout_ms:
+                raise RuntimeError(
+                    f"Timeout while waiting for RX DMA to finish on {self.read_page.__name__}. "
+                    f"Elapsed: {elapsed_ms} ms"
+                )
+
+        # finalize
+        sm0.active(0)
+        tx_dma0.close()
+        rx_dma0.close()
+        return rx_data
 
     def read_status(self, chip_index: int) -> int:
         pass
@@ -366,6 +406,9 @@ async def test_pio() -> None:
 
     for i, byte in enumerate(id):
         print(f"ID Byte {i}: {byte:02x}")
+
+    data = await pio_commander.read_page(cs=0, block=0, page=0, col=0)
+    print(f"Read Page Data: {list(data)}")
 
 
 async def main() -> None:
